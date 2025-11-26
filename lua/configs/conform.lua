a87a6005a0024aa234db883c10fdff6367ba1d86
aharabara <dev.xandr@gmail.com>
2025-11-26 22:17:48 +0300

    Latest updates, synced with latest nvchad

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,29 +4,29 @@ local options = {
     php = { "php" },
     css = { "prettier" },
     typescript = { "prettier" },
-    typescriptreact = {"prettier"},
+    typescriptreact = { "prettier" },
     javascript = { "prettier" },
-    javascriptreact = {"prettier"}
+    javascriptreact = { "prettier" },
     -- yaml = { "yamlfmt" },
   },
-    format_on_save = { 
-        lsp_fallback = true, 
-        async = true, 
-        timeout_ms = 1000, 
-    }, 
-    notify_on_error = true, 
-    formatters = { 
-        php= { 
-            command = "php-cs-fixer", 
-            args = { 
-                "fix", 
-                "$FILENAME",
-                -- "--config=/your/path/to/config/file/[filename].php",
-                "--allow-risky=yes", -- if you have risky stuff in config, if not you dont need it.
-            }, 
-            stdin = false,
-        }
-    }
+  format_on_save = {
+    lsp_fallback = true,
+    async = true,
+    timeout_ms = 1000,
+  },
+  notify_on_error = true,
+  formatters = {
+    php = {
+      command = "php-cs-fixer",
+      args = {
+        "fix",
+        "$FILENAME",
+        -- "--config=/your/path/to/config/file/[filename].php",
+        "--allow-risky=yes", -- if you have risky stuff in config, if not you dont need it.
+      },
+      stdin = false,
+    },
+  },
 
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
@@ -35,4 +35,4 @@ local options = {
   -- },
 }
 
-require("conform").setup(options)
+return options
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -89,9 +89,7 @@ return {
   {
     "stevearc/conform.nvim",
     event = "BufWritePre", -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    opts = require "configs.conform",
   },
   {
     "folke/trouble.nvim",
